e0f0a1e0a56b4c48bc8112791e405f9eb8fa3aff
peak11207 <peak11207@hotmail.com>
2024-09-06 16:52:58 +0700

    setting

---
+++ Controllers/EmployeesController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Backend_Developer_test.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly IEmployeeService _employeeService;
+
+        public EmployeesController(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+        // GET: api/<EmployeesController>
+        [HttpGet]
+        public Task<IActionResult> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<EmployeesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<EmployeesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+
+        }
+
+        // PUT api/<EmployeesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+
+        }
+
+        // DELETE api/<EmployeesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+
+        }
+    }
+}
---
+++ DATA/ApplicationDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Backend_Developer_test.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend_Developer_test.DATA
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+        {
+
+        }
+        public DbSet<Employee> Employee { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // การกำหนดความสัมพันธ์ระหว่าง Order และ OrderItem
+            modelBuilder.Entity<Employee>().ToTable("EmployeeTable");
+            //modelBuilder.Entity<Postmarker>().HasKey(m => m.Id);
+            modelBuilder.Entity<Employee>().HasKey(m => m.EmpNo);
+            modelBuilder.Entity<Employee>().Property(m => m.FirstName).IsRequired();
+            modelBuilder.Entity<Employee>().Property(m => m.LastName).IsRequired();
+            modelBuilder.Entity<Employee>().Property(m => m.Designation).IsRequired();
+            modelBuilder.Entity<Employee>().Property(m => m.HireDate).IsRequired();
+            modelBuilder.Entity<Employee>().Property(m => m.Salary).HasColumnType("float").IsRequired(); // Ensure the type matches the DB
+            modelBuilder.Entity<Employee>().Property(m => m.Comm);
+            modelBuilder.Entity<Employee>().Property(m => m.DeptNo).IsRequired(); 
+        }
+    }
+}
